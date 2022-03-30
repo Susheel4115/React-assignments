124c0da97e9b8b297fa3c45bdb4ddf5943dcf809
Mr.Susheel Kumar <suseelkumar4115@gmail.com>
2022-03-30 14:46:27 +0530

    This is first commit on assignment-2 react

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,18 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-
+import "./App.css";
 
 function App(props) {
- const {employee}=props;
-    
-return(
-  <div className="container">
-  {/* Start your React code here */}
-</div>
-)
+  const { employee } = props;
+
+  return (
+    <div className="container">
+      {/* Start your React code here */}
+      <img className="image1" src={employee.profileImg} />
+      <p className="name">{employee.name}</p>
+      <p className="location">Location</p>
+      <p className="realloc">{employee.location}</p>
+      <p className="bloodgroup">Blood group</p>
+      <p className="blood">{employee.bloodGroup}</p>
+      <p className="age">Age</p>
+      <p className="employeeage">{employee.age}</p>
+    </div>
+  );
 }
-
-
 
 export default App;
